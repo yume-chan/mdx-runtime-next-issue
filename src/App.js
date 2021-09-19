3aac7a0852069b8ee68ddcf48c5c9a74fd3047be
Simon Chan <1330321+yume-chan@users.noreply.github.com>
2021-09-19 14:10:59 +0800

    feat: add @mdx-js/runtime

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MDX from './Mdx';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <MDX />
       </header>
     </div>
   );
